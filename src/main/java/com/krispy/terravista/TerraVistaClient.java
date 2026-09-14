ac604ff67b7a04fc932707020c007d2a744a6638
Coolguy <mobilemc022@gmail.com>
2026-09-14 22:44:07 +0700

    Load configuration on client initialization

--- src/main/java/com/krispy/terravista/TerraVistaClient.java
+++ src/main/java/com/krispy/terravista/TerraVistaClient.java
@@ -1,10 +1,12 @@
 package com.krispy.terravista;
 
+import com.krispy.terravista.config.TerraVistaConfig;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TerraVistaClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        TerraVistaConfig.loadConfig();
     }
 }
