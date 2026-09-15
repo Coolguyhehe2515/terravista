84a4e140783fede3c91eb7a2fd74bfea6035c6c8
Coolguy <mobilemc022@gmail.com>
2026-09-15 12:53:04 +0700

    Create TerraVistaModMenu.java

---
+++ src/main/java/com/krispy/terravista/TerraVistaModMenu.java
@@ -0,0 +1,14 @@
+package com.krispy.terravista;
+
+import com.krispy.terravista.config.TerraVistaConfigScreen;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
+
+public class TerraVistaModMenu implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return TerraVistaConfigScreen::create;
+    }
+}
