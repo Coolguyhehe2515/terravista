c0a1bd62a05ac9b0017426fc091f0d3189ecbb83
Coolguy <mobilemc022@gmail.com>
2026-09-15 13:28:15 +0700

    Update LodMesh.java

--- src/main/java/com/krispy/terravista/lod/LodMesh.java
+++ src/main/java/com/krispy/terravista/lod/LodMesh.java
@@ -1,12 +1,10 @@
 package com.krispy.terravista.lod;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexBuffer;
+import net.minecraft.client.gl.VertexBuffer;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
-import org.joml.Matrix4f;
 
 public class LodMesh implements AutoCloseable {
 
@@ -14,16 +12,11 @@ public class LodMesh implements AutoCloseable {
     private boolean uploaded;
 
     public LodMesh() {
-        this.vertexBuffer = new VertexBuffer(VertexBuffer.Usage.STATIC);
-        this.uploaded = false;
+        vertexBuffer = new VertexBuffer(VertexBuffer.Usage.STATIC);
+        uploaded = false;
     }
 
-    public void build(
-            LodChunk chunk,
-            double cameraX,
-            double cameraY,
-            double cameraZ
-    ) {
+    public void build(LodChunk chunk) {
         Tessellator tessellator = Tessellator.getInstance();
 
         BufferBuilder buffer = tessellator.begin(
@@ -41,43 +34,51 @@ public class LodMesh implements AutoCloseable {
                 int worldZ = chunk.getWorldZ(z);
                 int height = chunk.getHeight(x, z);
 
-                float r = ((chunk.getColor(x, z) >> 16) & 255) / 255.0f;
-                float g = ((chunk.getColor(x, z) >> 8) & 255) / 255.0f;
-                float b = (chunk.getColor(x, z) & 255) / 255.0f;
+                int color = chunk.getColor(x, z);
 
-                float x1 = (float) (worldX - cameraX);
-                float z1 = (float) (worldZ - cameraZ);
-                float x2 = (float) (worldX + step - cameraX);
-                float z2 = (float) (worldZ + step - cameraZ);
-                float y = (float) (height - cameraY);
+                float r = ((color >> 16) & 255) / 255.0f;
+                float g = ((color >> 8) & 255) / 255.0f;
+                float b = (color & 255) / 255.0f;
 
-                buffer.vertex(x1, y, z1)
+                buffer.vertex(worldX, height, worldZ)
                         .color(r, g, b, 1.0f);
 
-                buffer.vertex(x1, y, z2)
-                        .color(r, g, b, 1.0f);
+                buffer.vertex(
+                        worldX,
+                        height,
+                        worldZ + step
+                ).color(r, g, b, 1.0f);
 
-                buffer.vertex(x2, y, z2)
-                        .color(r, g, b, 1.0f);
+                buffer.vertex(
+                        worldX + step,
+                        height,
+                        worldZ + step
+                ).color(r, g, b, 1.0f);
 
-                buffer.vertex(x2, y, z1)
-                        .color(r, g, b, 1.0f);
+                buffer.vertex(
+                        worldX + step,
+                        height,
+                        worldZ
+                ).color(r, g, b, 1.0f);
             }
         }
 
         BufferBuilder.BuiltBuffer builtBuffer = buffer.end();
 
-        if (builtBuffer != null) {
-            vertexBuffer.bind();
-            vertexBuffer.upload(builtBuffer);
-            VertexBuffer.unbind();
-            builtBuffer.close();
-
-            uploaded = true;
+        if (builtBuffer == null) {
+            return;
         }
+
+        vertexBuffer.bind();
+        vertexBuffer.upload(builtBuffer);
+        VertexBuffer.unbind();
+
+        builtBuffer.close();
+
+        uploaded = true;
     }
 
-    public void draw(Matrix4f matrix) {
+    public void draw() {
         if (!uploaded) {
             return;
         }
